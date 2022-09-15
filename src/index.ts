f9300cccc928753fcbf774518b1a4d36403205dd
SeungHo0422 <songseungho9258@gmail.com>
2022-09-16 04:29:08 +0900

    inde.ts 예제코드 작성

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log('Hellddo');
-console.log('whyrano');
+import express = require('express');
+import { Request, Response, NextFunction, Application } from 'express';
+
+const app: Application = express();
+const prod: boolean = process.env.NODE_ENV === 'production';
+
+app.get('/', (req, res) => {
+  res.send('node.js boilerplate 정상 동작!');
+});
+
+app.listen(prod ? process.env.PORT : 3065, () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
